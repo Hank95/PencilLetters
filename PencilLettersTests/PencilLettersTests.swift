e84d8a4a153afed8de54e2112ef3e22e9c4fb69e
Henry Pendleton <hhpendleton@gmail.com>
2025-07-05 15:39:21 -0400

    Initial Commit

---
+++ PencilLettersTests/PencilLettersTests.swift
@@ -0,0 +1,17 @@
+//
+//  PencilLettersTests.swift
+//  PencilLettersTests
+//
+//  Created by Henry Pendleton on 7/5/25.
+//
+
+import Testing
+@testable import PencilLetters
+
+struct PencilLettersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
